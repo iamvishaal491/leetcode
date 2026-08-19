f69bf4983159d30d32971f511171b34c29a0278d
iamvishaal491 <vishaalramesh491@gmail.com>
2026-08-19 10:40:36 +0000

    Solved 1342-number-of-steps-to-reduce-a-number-to-zero - LeetSync

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int c=0;
+        while(num>0){
+            if (num%2==0) {
+                num=num/2;
+                c+=1;
+            }
+            else{
+                num=num-1;
+                c+=1;
+            }
+        }
+        return c;
+        
+    }
+}
