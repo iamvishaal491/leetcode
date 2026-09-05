606f2952358ef9f9b591fca88bf23b47574905b3
iamvishaal491 <vishaalramesh491@gmail.com>
2026-09-05 09:16:06 +0000

    Solved 700-search-in-a-binary-search-tree - LeetSync

---
+++ 700-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -0,0 +1,6 @@
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        while root and root.val!=val:
+            root= root.left if val<root.val else root.right
+        return root
+        
